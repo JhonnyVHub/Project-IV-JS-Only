56d4d073a136a35fa5941d102261b2af6655d778
unknown <mjhonnyvieira@gmail.com>
2023-08-07 20:14:00 -0300

    Desafio JS

---
+++ js/script.js
@@ -0,0 +1,90 @@
+// let nom = prompt("Diga seu nome");
+// alert("Bem vindo ao sistema, " + nom);
+
+// // acima está a atv1
+
+// let nom = prompt("Diga seu nome");
+// let ida = prompt("Qual sua idade?");
+// alert(nom + " você tem " + ida + " anos");
+
+// acima está a atv2
+
+// let pob = 214000000;
+// alert("População brasileira: " + pob);
+// let poc = 1412000000;
+// alert("População chinesa: " + poc);
+// if (poc > pob){
+//     alert("A população chinesa é maior que a brasileira")
+// }
+// else if(pob > poc){
+//     alert("A população brasileira é maior que a chinesa")
+// };
+
+// acima está a atv3
+
+// let val = prompt("Qual o número escolhido?");
+// let por = val % 2;
+// if(por == 0){
+//     alert("Seu número é Par!")
+// }
+// else if(por == 1){
+//     alert("Seu número é Ímpar!")
+// };
+
+// acima está a atv4
+
+// let sal = prompt("Seu salário");
+// let des = parseFloat(prompt("Desconto estabelecido"));
+// let des1 = (des / 100);
+// let fin = parseInt((sal * des1));
+// alert("Seu salário é R$" + fin + ",00");
+
+// acima está a atv5
+
+// let cel = prompt("Temperatura");
+// let tem = (cel * 9/5) + 32;
+// alert("A temperatura convertida é " + tem + "°F");
+
+// acima está a atv6
+
+// let va1 = parseFloat(prompt("Primeiro Valor"));
+// let va2 = parseFloat(prompt("Segundo Valor"));
+// let va3 = parseFloat(prompt("Terceiro Valor"));
+// if (va1 > va2 && va1 > va3) {
+//     alert("O Primeiro valor é o maior!")
+// }
+// else if(va2 > va1 && va2 > va3) {
+//     alert("O Segundo valor é o maior!")
+// }
+// else if(va3 > va1 && va3 > va2) {
+//     alert("O Terceiro valor é o maior!")
+// };
+
+// acima está a atv7
+
+// let sida = parseInt(prompt("Sua idade"));
+// let ano = parseInt(2023);
+// let anns = (ano - sida);
+// alert("Você nasceu em " + anns);
+
+// acima está a atv8
+
+
+// let pes = parseFloat(prompt("Seu peso"));
+// let alt = parseFloat(prompt("Sua altura"));
+// let frm = pes / (alt ** 2);
+// if(frm < 16.9){
+//     alert("Muito abaixo do peso")
+// }
+// else if(frm > 16.9 && frm < 18.9){
+//     alert("Abaixo do peso")
+// }
+// else if(frm > 18.9 && frm < 26.9){
+//     alert("normal")
+// }
+// else if(frm > 26.9 && frm < 31.9){
+//     alert("Acima do Peso")
+// }
+// else if(frm > 31.9){
+//     alert("Obesidade")
+// };
